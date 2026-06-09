bf283c2c9bb73c06a24a7a95145415265d984d32
perumal934583368-jpg <perumal934583368@gmail.com>
2026-06-09 15:59:03 +0100

    Add files via upload

---
+++ anusha.py
@@ -0,0 +1,73 @@
+"""anu=float(input("enter your number:"))
+indhu=float(input("enter your number:"))
+agalya=float(input("enter your number:"))
+average =(anu+indhu+agalya)/3
+print(average)"""
+"""r=int(input("enteryour radius:"))
+circle=3.14*r*r
+print(circle)"""
+"""s=float(input("enter your square:"))
+square=s*s
+print(square)"""
+"""b=float(input("enter your base :"))
+h=float(input("enter your hight:"))
+triangle=0.5*b*h
+print(triangle)"""
+"""c=float(input("enter your temperature:"))
+fahrenheit=(c*9/5)+32
+print(fahrenheit)"""
+"""f=float(input("enter your fahrenheit:"))
+celsius=(f-32)*5/9
+print(celsius)"""
+"""p=float(input("enter your amount:"))
+r=float(input("enter rate of interset:"))
+t=float(input("enter time:"))
+total=(p*r*t)/100
+print(total)"""
+"""p=int(input("enter your amount:"))
+i=float(input("rate of interset:"))
+t=float(input("time:"))            
+com=p*(1+i/100)**t
+print(com)"""
+"""a=10
+b=20
+c=30
+a=(c+b)
+print(a)"""
+"""a=10+20
+b=20-10
+a=a-b
+b=a+b
+print(b-a)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

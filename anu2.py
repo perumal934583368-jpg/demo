4412fd2fdba29f56e264d7867917b28e6283b6d0
perumal934583368-jpg <perumal934583368@gmail.com>
2026-06-15 18:01:54 +0100

    Add files via upload

---
+++ anu2.py
@@ -0,0 +1,142 @@
+"""password=int(input("enter your password"))
+reg_password=2003
+if(password==reg_password):
+    print("your password is correct")
+    amount=int(input("enter your amount"))
+    balance=5000
+    print("avilable balance=",balance)
+    if(amount<=balance):
+        balance=balance-amount
+        print("take your amount")
+        print("avilable balence=",balance)
+        savings=int(input("enter your savings"))
+        if(savings<=2000):
+            print("eligible for savings")
+        else:
+             print("minimum savings amount is 2000")
+    else:
+         print("insufficient amount")
+else:
+     print("invalied amount")"""
+"""a=int(input("enter your length:"))
+b=int(input("enter your breadh:"))
+if a==b:
+  print("square")
+else:
+    print("rectangle")"""
+"""
+a=int(input("enter your number:"))
+b=a*a
+if b>=50:
+  print("square is above 50")
+else:
+  print("sequare is below 50")"""
+"""
+a=int(input("enter your number:"))
+b=int(input("enter your number:"))
+c=a-b
+if c==0:
+    print("the difference is 0")
+else:
+    print("the difference is not 0")"""
+"""
+a=int(input("enter your computer science mark:"))
+if (a>=50):
+    print("pass mark")
+else:
+    print("fail mark")"""
+"""a=int(input("enter your number:"))
+dight1=a//10
+dight2=a%10
+if dight1>=dight2:
+    print("dight1")
+else:
+    print("dight2")"""
+
+
+"""a=int(input("enter your value:"))
+if a==1:
+    print("you can go of play")
+else:
+    print("you cannot go of play")"""
+
+"""a=int(input("enter your number:"))
+b=int(input("enter your number:"))
+c=int(input("mathematical operator:"))
+if c=="+":
+    print("+=",a+b)
+elif c=="-":
+    print("-=",a-b)
+elif c=="*":
+    print("*=",a*b)
+elif c=="/":
+    print("/=",a/b)"""
+"""
+a=35
+b=60
+c=90
+print("a.35")
+print("b.60")
+print("c.90")
+mark=int(input("enter your mark:"))
+if(mark==a):
+    print("the exam will be difficult")
+elif(mark==b):
+    print("the exam is normal")
+elif(mark==c):
+    print("the exam will be easy
+ else:
+    print("invalied mark")"""
+
+"""weight=float(input("enter weight of parcel:"))
+booking=(input("enter your booking type,(o/e):"))
+if booking=="o":
+    if weight<=5:
+        print("charge=50")
+    elif weight<=10:
+        print("charge=100")
+    elif weight<=15:
+        print("charge=150")
+    else:
+        print("charge=200")
+    
+elif booking=="e":
+    if weight<=5:
+        print("charge=500")
+    elif weight<=10:
+        print("charge=1000")
+    elif weight<=15:
+        print("charge 1500")
+    else:
+        print("charge 2000")
+else:
+    print("invalied booking ")"""
+"""price=float(input("enter the price the laptop:"))
+if price<=50000:
+   print("discount=price*0.10")
+elif price<=75000:
+     print ("discount=price*0.15")
+else:
+    discount = price*0.15
+    total_price = price-discount
+    print("price of laptop:",price)
+    print("price of discount:",discount)
+    print("total price:",total_price)"""
+
+    
+
+
+
+
+              
+   
+
+
+
+
+
+
+
+
+
+    

c708e2656a407e1ed20459d9579af796e1ef0c01
perumal934583368-jpg <perumal934583368@gmail.com>
2026-06-22 06:55:55 +0100

    Update anu3.py

--- anu3.py
+++ anu3.py
@@ -1,29 +1,38 @@
-"""for i in range(1,10):#rows
-    for j in range (1,10):#columns
-        if i== 1 and j<10:
-          print("@ ",end="")
-        elif i<10 and j==1:
-         print("@ ",end="")
-        else:
-            print(j,"",end="")
+"""language=["english","tamil","french","hindi"]
+#print(language[0])
+#print(language[1])
+language[3]="telgu"
+print(language[3])"""
+"""for i in "thars":
+    print(i,end="")"""
+"""for i in range (1,8):
+    for j in range (1,i):
+        print("",end="")
+    for k in range (i,8):
+        print("*",end="")
     print()"""
-"""for i in range(1,4):#rows
- for j in range (1,6):#columns
-   if i==1 and j==3:
-       print("@ ",end="")
-   elif i==2 and j==2:
-       print("@ ",end="")
-   elif i==2 and j==3:
-       print("@ ",end="")
-   elif i==2 and j==4:
-       print("@ ",end="")
-   elif i==3 and j==1:
-       print("@ ",end="")
-   elif i==3 and j==5:
-       print("@  ",end="")
-   else:
-       print(""," ",end="")
- print()"""
-n=int(input("enter your table:"))
-for i in range(1,11):
-   print(i,"*",n,"=",i*n)
+"""for i in [1,2,3,2,1]:
+    for j in range (i,4):
+        print(" ",end="")
+    for k in range(i):
+        print("* ",end="")
+    print()"""
+"""f=int(input("enter the number"))
+sum_even=0
+sum_odd=0
+for i in range(1,f):
+    if i%2==0:
+        sum_even+=i
+    else:
+        sum_odd+=i
+print("sum_even+=i",sum_even)
+print("sum_odd+=i",sum_odd)"""
+"""num=int(input("enter the number"))
+fact=1
+i=1
+while i<=num:
+  fact=fact*i
+  i+=1
+  
+print("factorial=",fact)"""
+  
